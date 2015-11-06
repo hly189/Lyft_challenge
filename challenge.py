79877eb14ba81b92a41a9756435e7e7cde6ce920
Hoa Ly <lythaihoa2311992@gmail.com>
2015-11-06 10:29:52 -0800

    Lyft Challenge using Haversine formula

--- challenge.py
+++ challenge.py
@@ -6,8 +6,8 @@ class Solution():
 
 #Take coordinate of a point 
 	def coordinate(self,longtitude,latitude): 
-		self.longtitude = longtitude 
-		self.latitude = latitude
+		self.longtitude = self.to_radian(longtitude) 
+		self.latitude = self.to_radian(latitude)
 
 #Change the coordinate to radians
 	def to_radian(self,num): 
@@ -15,12 +15,12 @@ class Solution():
 
 #Calculate \phi_2 - \phi_1 and \lambda_2 - \lambda_1 in Haversine function
 	def distance(self,x,y): 
-		self.diff_lat = self.to_radian(y.latitude - x.latitude)
-		self.diff_long = self.to_radian(y.longtitude - x.longtitude)
+		self.diff_lat = y.latitude - x.latitude
+		self.diff_long = y.longtitude - x.longtitude
 
 #Calculate the distance using Haversine function
 	def Haversine(self,x,y): 
-		self.a = math.pow(math.sin(self.diff_lat/2),2) + math.cos(self.to_radian(x.latitude))*math.cos(self.to_radian(y.latitude))*math.pow(self.diff_lat/2,2)
+		self.a = math.pow(math.sin(self.diff_lat/2),2) + math.cos(x.latitude)*math.cos(y.latitude)*math.pow(math.sin(self.diff_long/2),2)
 		self.distance = 2*self.radius*math.asin(math.sqrt(self.a))
 		return self.distance
 
